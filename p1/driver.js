d18a4676772cbc8ac81548900d295e647ba5fa07
Nil patel <nilpatel0606@gmail.com>
2022-04-14 10:08:30 +0530

    Change driver.js

--- p1/driver.js
+++ p1/driver.js
@@ -1,6 +1,6 @@
 const {add,div,mul,sub}= require("./calculator");
 
 console.log(`addition  of 12 and 32      :  ${add(12,32)}`);
-console.log(`multiplication of 12 and 32 :  ${add(12, 32)}`);
-console.log(`subtraction of 12 and 32    :  ${add(12, 32)}`);
-console.log(`division of 12 and 32       :  ${add(12, 32)}`);
+console.log(`multiplication of 12 and 32 :  ${mul(12, 32)}`);
+console.log(`subtraction of 12 and 32    :  ${sub(12, 32)}`);
+console.log(`division of 12 and 32       :  ${div(12, 32)}`);
